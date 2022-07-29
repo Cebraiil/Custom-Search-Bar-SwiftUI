9a6b01111494a3d9f942a2a4810c2109c034591d
developerlight <abdul.adl@icloud.com>
2022-07-29 18:42:34 +0300

    Initial Commit

---
+++ CustomSearchBar/CustomSearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSearchBarApp.swift
+//  CustomSearchBar
+//
+//  Created by devlight on 29.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomSearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
