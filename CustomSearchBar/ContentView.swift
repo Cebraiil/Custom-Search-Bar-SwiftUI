a3fbb6866d63580d9a3b97f907daafeee7bc6ecc
developerlight <abdul.adl@icloud.com>
2022-07-29 19:47:08 +0300

    Search bar codes added.

--- CustomSearchBar/ContentView.swift
+++ CustomSearchBar/ContentView.swift
@@ -2,15 +2,34 @@
 //  ContentView.swift
 //  CustomSearchBar
 //
-//  Created by devlight on 29.07.2022.
+//  by devlight on 29.07.2022.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    let languages = ["JavaScript", "Python", "SwiftUI", "Dart"]
+    
+    @State private var searchText = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(searchResult, id: \.self) { language in
+                    Text(language)
+                }
+            }
+            .searchable(text: $searchText)
+            .navigationTitle("Languages")
+        }
+    }
+    
+    var searchResult: [String] {
+        if searchText.isEmpty {
+            return languages
+        } else {
+            return languages.filter { $0.contains(searchText)}
+        }
     }
 }
 
